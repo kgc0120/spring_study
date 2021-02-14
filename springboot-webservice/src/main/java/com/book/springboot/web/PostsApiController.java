7d8ad94172d81dc2b7cc4a1f1cc4373312e06498
kgc0120 <senru0508@gmail.com>
2021-02-14 21:43:52 +0900

    git test

--- springboot-webservice/src/main/java/com/book/springboot/web/PostsApiController.java
+++ springboot-webservice/src/main/java/com/book/springboot/web/PostsApiController.java
@@ -16,6 +16,10 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+
+
+
         return postsService.save(requestDto);
     }
 
