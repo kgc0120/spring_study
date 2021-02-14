c72b9354498cb602bc3953554b217c8588a90eba
kgc0120 <senru0508@gmail.com>
2021-02-14 23:16:45 +0900

    get, update test code

--- springboot-webservice/src/main/java/com/book/springboot/service/posts/PostsService.java
+++ springboot-webservice/src/main/java/com/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.book.springboot.service.posts;
 
+import com.book.springboot.domain.posts.Posts;
 import com.book.springboot.domain.posts.PostsRepository;
+import com.book.springboot.web.dto.PostsResponseDto;
 import com.book.springboot.web.dto.PostsSaveRequestDto;
+import com.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,4 +19,21 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                                    .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                                        .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
 }
