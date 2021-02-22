bb7f9a6de53abc76b77c53469abd6a0819e1df64
kgc0120 <senru0508@gmail.com>
2021-02-22 23:34:43 +0900

    config spring session

--- springboot-webservice/src/main/java/com/book/springboot/web/IndexController.java
+++ springboot-webservice/src/main/java/com/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.book.springboot.web;
 
+import com.book.springboot.config.auth.LoginUser;
 import com.book.springboot.config.auth.dto.SessionUser;
 import com.book.springboot.domain.user.User;
 import com.book.springboot.service.posts.PostsService;
@@ -20,9 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
